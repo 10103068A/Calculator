8cb1f321a59ac58c22bd6ee7d2ce42026f389f0d
郭姵萱 <paigekuo@moneydj.com>
2019-11-21 12:48:02 +0800

    簡易V1

---
+++ Calculator/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        double oneNum, twoNum, ansNun = 0;
+        string Operator = "";
+        bool isNewNum= true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void operator_Click(object sender, EventArgs e)
+        {
+            if (Operator != "")
+            {
+                twoNum = double.Parse(textBox1.Text);
+                Pro();
+            }
+            Button b = sender as Button;
+            Operator = b.Text;
+            isNewNum = true;
+            oneNum = double.Parse(textBox1.Text);
+        }
+
+        private void number_Click(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            if (isNewNum)
+            {
+                textBox1.Text = b.Text;
+                isNewNum = false;
+            }
+            else
+            {
+                textBox1.Text += b.Text;
+            }
+        }
+
+        private void equal_Click(object sender, EventArgs e)
+        {
+            if (Operator != "")
+            {
+                twoNum = double.Parse(textBox1.Text);
+                Pro();
+            }
+            isNewNum = true;
+        }
+
+        private void AC_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            oneNum = 0;
+            twoNum = 0;
+            ansNun = 0;
+            Operator = "";
+        }
+
+        private void point_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.ToString().IndexOf(".") == -1)
+            {
+                textBox1.Text += ".";
+            }
+        }
+
+        private void Pro()
+        {
+            switch (Operator)
+            {
+                case "+":
+                    ansNun = oneNum + twoNum;
+                    break;
+                case "-":
+                    ansNun = oneNum - twoNum;
+                    break;
+                case "*":
+                    ansNun = oneNum * twoNum;
+                    break;
+                case "/":
+                    ansNun = oneNum / twoNum;
+                    break;
+            }
+            textBox1.Text = ansNun.ToString();
+        }
+    }
+}
